77c98ae5a2443b0dddb06d76d6d11437638acd57
webhead1104 <webhead1104@hotmail.com>
2025-07-16 10:19:19 -0500

    Use unused

--- src/main/java/net/hollowcube/schem/Schematic.java
+++ src/main/java/net/hollowcube/schem/Schematic.java
@@ -19,6 +19,7 @@ import java.util.List;
 /**
  * <p>A publicly immutable copy of any loaded schematic. This interface defines the basic requirements for any schematic.</p>
  */
+@SuppressWarnings("unused")
 public interface Schematic {
 
     static @NotNull Schematic empty() {
@@ -35,12 +36,14 @@ public interface Schematic {
     default @Nullable String name() {
         return null;
     }
+
     /**
      * Returns the author of the schematic, if available.
      */
     default @Nullable String author() {
         return null;
     }
+
     /**
      * Returns the creation time of the schematic, if available.
      */
@@ -49,6 +52,7 @@ public interface Schematic {
     }
 
     @NotNull Point size();
+
     default @NotNull Point offset() {
         return Vec.ZERO;
     }
@@ -64,12 +68,15 @@ public interface Schematic {
     default @NotNull RelativeBlockBatch createBatch() {
         return createBatch(Rotation.NONE, null);
     }
+
     default @NotNull RelativeBlockBatch createBatch(@NotNull BlockTransformer blockTransformer) {
         return createBatch(Rotation.NONE, blockTransformer);
     }
+
     default @NotNull RelativeBlockBatch createBatch(@NotNull Rotation rotation) {
         return createBatch(rotation, null);
     }
+
     default @NotNull RelativeBlockBatch createBatch(@NotNull Rotation rotation, @Nullable BlockTransformer blockTransformer) {
         RelativeBlockBatch batch = new RelativeBlockBatch(new BatchOption().setCalculateInverse(true));
         forEachBlock(rotation, (pos, block) -> {
@@ -88,6 +95,7 @@ public interface Schematic {
     default boolean hasBlockData() {
         return false;
     }
+
     /**
      * Returns the block palette for the entire schematic, zero indexed with no empty spaces.
      *
@@ -100,6 +108,7 @@ public interface Schematic {
     default @NotNull List<Block> blockPalette() {
         return List.of();
     }
+
     /**
      * Returns the block data for the entire schematic. The format is (size.x * size.y * size.z) var ints
      * in a row, each corresponding to an entry in {@link #blockPalette()}.
@@ -113,6 +122,7 @@ public interface Schematic {
     default @NotNull ByteArrayBinaryTag blockData() {
         return SpongeSchematic.EMPTY_BYTE_ARRAY;
     }
+
     default @NotNull Collection<BlockEntityData> blockEntities() {
         return List.of();
     }
@@ -120,9 +130,11 @@ public interface Schematic {
     default boolean hasBiomeData() {
         return false;
     }
+
     default @NotNull List<String> biomePalette() {
         return List.of();
     }
+
     default @NotNull ByteArrayBinaryTag biomeData() {
         return SpongeSchematic.EMPTY_BYTE_ARRAY;
     }
